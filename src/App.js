d79336db985464a9882aafdafabedaab6936fc33
Octavio Martinez <omartinez1618@gmail.com>
2022-10-28 22:41:29 -0400

    qr ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {QRCodeCanvas} from 'qrcode.react';
+import { useState } from 'react';
 function App() {
+  const [value, setValue] = useState('')
+  const [qrlist, setQrlist] = useState([])
+
+  const changeEventHandler = ({target}) =>{
+    setValue(prevValue => target.value)
+  }
+
+  const clickEventHandler = ()=>{
+    setQrlist(prevQrlist => [...prevQrlist, value])
+    setValue( prevValue => '' )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display:'flex', flexDirection:'row'}}>
+      <div className="App" style={{
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center',
+        height: '100vh',
+        width:'80%'
+      }}>
+          <QRCodeCanvas value={value} />
+          
+            <input type='text' value={value}  style={{margin:'10px'}} onChange={changeEventHandler}/>
+            <button onClick={clickEventHandler}>Agregar</button>
+
+          
+      </div>
+      <div style={{width:'20%', borderLeft:'1px solid #030303'}}>
+        {
+          qrlist.map( item => (<div style={{display:'flex', alignItems:'center', margin:'10px', justifyContent:'space-between'}}><QRCodeCanvas style={{width:'64px', height:'64px'}} value={item} /> <i>{item}</i></div>))
+        }
+      </div>
     </div>
   );
 }
